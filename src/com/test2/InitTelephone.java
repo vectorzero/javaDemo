5b0f421a6d69a7ed2e317a408ed886e23e4a185f
vectorzero <w3298028@163.com>
2018-04-18 00:03:39 +0800

    无参的构造方法

---
+++ src/com/test2/InitTelephone.java
@@ -0,0 +1,7 @@
+package com.test2;
+
+public class InitTelephone {
+    public static void main(String[] args) {
+        Telephone phone = new Telephone();
+    }
+}
---
+++ src/com/test2/Telephone.java
@@ -0,0 +1,8 @@
+package com.test2;
+
+public class Telephone {
+    //构造方法
+    public Telephone() {
+        System.out.println("无参的构造方法执行了");
+    }
+}
