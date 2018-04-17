f8e4143954a95cc83486bb6c22e8bde18a029318
vectorzero <w3298028@163.com>
2018-04-18 00:25:34 +0800

    构造方法的重载； 构造方法可以保证对象属性赋一个合理的值；

--- src/com/test2/InitTelephone.java
+++ src/com/test2/InitTelephone.java
@@ -3,8 +3,8 @@ package com.test2;
 public class InitTelephone {
     public static void main(String[] args) {
         //无参的构造方法可以创建对象
-        Telephone phone = new Telephone();
+        //Telephone phone = new Telephone();
         //有参的构造方法也可以创建对象，并给对象中的实例赋初始值
-        Telephone phone2 = new Telephone(5.0f,1.4f,2.0f);
+        Telephone phone2 = new Telephone(1.5f,1.4f,2.0f);
     }
 }
--- src/com/test2/Telephone.java
+++ src/com/test2/Telephone.java
@@ -1,16 +1,21 @@
 package com.test2;
 
 public class Telephone {
-    float screen;
-    float cpu;
-    float mem;
+    private float screen;
+    private float cpu;
+    private float mem;
     //无参的构造方法
     public Telephone() {
         System.out.println("无参的构造方法执行了");
     }
     //有参的构造方法
     public Telephone(float newScreen,float newCpu, float newMem) {
-        screen = newScreen;
+        if(newScreen < 3.5f) {
+            System.out.println("您输入的值有问题");
+            screen = 3.5f;
+        }else {
+            screen = newScreen;
+        }
         cpu = newCpu;
         mem = newMem;
         System.out.println("有参的构造方法执行了");
