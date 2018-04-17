e44d67ff766ed08814661671594bb75f07a07f98
vectorzero <w3298028@163.com>
2018-04-18 00:15:05 +0800

    有参的构造方法

--- src/com/test2/InitTelephone.java
+++ src/com/test2/InitTelephone.java
@@ -2,6 +2,9 @@ package com.test2;
 
 public class InitTelephone {
     public static void main(String[] args) {
+        //无参的构造方法可以创建对象
         Telephone phone = new Telephone();
+        //有参的构造方法也可以创建对象，并给对象中的实例赋初始值
+        Telephone phone2 = new Telephone(5.0f,1.4f,2.0f);
     }
 }
--- src/com/test2/Telephone.java
+++ src/com/test2/Telephone.java
@@ -1,8 +1,18 @@
 package com.test2;
 
 public class Telephone {
-    //构造方法
+    float screen;
+    float cpu;
+    float mem;
+    //无参的构造方法
     public Telephone() {
         System.out.println("无参的构造方法执行了");
+    }
+    //有参的构造方法
+    public Telephone(float newScreen,float newCpu, float newMem) {
+        screen = newScreen;
+        cpu = newCpu;
+        mem = newMem;
+        System.out.println("有参的构造方法执行了");
     }
 }
