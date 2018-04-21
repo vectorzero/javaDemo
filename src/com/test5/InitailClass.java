f35fb295e354cb81981ad0c71cd963aecb707265
vectorzero <w3298028@163.com>
2018-04-21 10:56:13 +0800

    继承

---
+++ src/com/test5/InitailClass.java
@@ -0,0 +1,10 @@
+package com.test5;
+
+public class InitailClass {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.age = 18;
+        dog.name = "旺财";
+        dog.eat();
+    }
+}
