2b5ed33c6b773716e09c1b5117c06a14185b17ef
vectorzero <w3298028@163.com>
2018-04-21 10:49:42 +0800

    继承

---
+++ src/com/test5/Dog.java
@@ -0,0 +1,10 @@
+package com.test5;
+
+public class Dog extends Animal {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.age = 18;
+        dog.name = "旺财";
+        dog.eat();
+    }
+}
