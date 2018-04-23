488663f5b19fb6d1712489b3cfdc21696c72df1e
vectorzero <w3298028@163.com>
2018-04-24 00:18:04 +0800

    equals()方法

--- src/com/test5/InitailClass.java
+++ src/com/test5/InitailClass.java
@@ -6,9 +6,19 @@ public class InitailClass {
         Animal animal = new Animal();
         System.out.println("Animal的age:"+animal.age);
         Dog dog = new Dog();
-        dog.name = "旺财";
-        dog.eat();
-        dog.method();
+        dog.age = 15;
+        Dog dog2 = new Dog();
+        dog2.age = 15;
+        //equals()方法
+        //重写前不相等，重写后相等
+        if(dog.equals(dog2)) {
+            System.out.println("两个对象相等");
+        }else {
+            System.out.println("两个对象不相等");
+        }
+//        dog.name = "旺财";
+//        dog.eat();
+//        dog.method();
         // 继承Object类
         // 返回对象的哈希code码(对象地址字符串)
         // 如果直接输出对象，就会执行toString()方法
